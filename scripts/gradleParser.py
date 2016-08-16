b644ace1ff1576e45ebd493be869c688f076b7cf
Yigit Ozgumus <yigitozgumus1@gmail.com>
2016-08-16 22:43:40 +0300

    gradle parser is implemented. Nested structure and duplicate keys for the dependencies section will be implemented.

--- scripts/gradleParser.py
+++ scripts/gradleParser.py
@@ -2,12 +2,13 @@
 
 from checkUtil import working_directory
 
+
 class GradleParser(object):
     """
 
     This class will take a gradle file and returns a parsed Dictionary format
     """
-    dictionary_list = list()
+    dictionary_list = ["outside"]
     gradle_dict = dict()
 
 
@@ -19,18 +20,27 @@ class GradleParser(object):
         current_entry = object()
         for line in self.file:
             if line in ['\n', '\r\n']:
-                print "empty line"
+                pass
+            if "//" in line:
+                pass
             elif "{" in line.strip():
                 arg,sep = line.strip().split(" ")
                 self.dictionary_list.append(arg)
                 self.gradle_dict[arg] = dict()
                 current_entry = self.gradle_dict[arg]
             elif "{" and "}" not in line.strip():
+                if(len(self.gradle_dict) == 0):
+                    self.gradle_dict["outside"] = dict()
                 current_entry = self.gradle_dict[self.dictionary_list[len(self.dictionary_list)-1]]
-                arg,sep = line.strip().split(" ")
-                current_entry[arg] = sep
+                args = line.strip().split(" ")
+                if(len(args) == 2):
+                    current_entry[args[0]] = args[1]
+                elif(len(args)> 2):
+                    for i in range(1,len(args)):
+                        current_entry[args[0]] = args[i]
             elif "}" in line.strip():
                 current_entry = self.dictionary_list.pop()
+
         return self.gradle_dict
 
 
