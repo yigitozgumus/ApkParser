0c063b7d4b3a09e3fb1a9c05089c1fc4427db73d
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2016-08-16 08:57:03 +0300

    initializer script update

--- scripts/initializer.py
+++ scripts/initializer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
-import urllib2
+
 from subprocess import call
-import os
 from contextlib import contextmanager
 from apk_parse.apk import APK
+import os
+import urllib2
+import sys,getopt
 
+# Context manager function for changing directory if necessary
 @contextmanager
 def working_directory(directory):
     owd = os.getcwd()
@@ -15,73 +18,111 @@ def working_directory(directory):
     finally:
         os.chdir(owd)
 
-scriptFile = urllib2.urlopen("https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/osx/apktool")
-jarFile = urllib2.urlopen("https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.0.jar")
-filename_script = "apktool"
-filename_jar = "apktool.jar"
-
-with working_directory("/tmp"):
-    print "Downloading script..."
-    with open(filename_script, "wb") as output_script:
-        output_script.write(scriptFile.read())
-    print "Downloading jar file..."
-    with open(filename_jar, "wb") as output_jar:
-        output_jar.write(jarFile.read())
-    call(["mv",filename_script,"/usr/local/bin"])
-    call(["mv", filename_jar, "/usr/local/bin"])
-
-with working_directory("/usr/local/bin"):
-    print "Changing the mod of the script..."
-    call(["chmod","+x",filename_script])
-    print "Changing the mod of the jar..."
-    call(["chmod", "+x", filename_jar])
-
-apk_location = "/users/ozgumusy/AndroidStudioProjects/RijksClient/app/app-release.apk"
-project_location = "/users/ozgumusy/AndroidStudioProjects/RijksClient"
-
-with working_directory("/tmp"):
-    call(["apktool","d",apk_location])
-# Print AndroidManifest.xml file
-with working_directory("/tmp" + "/app-release" ):
-    f = open("AndroidManifest.xml","rw")
-    for line in f:
-        print line,
+def main():
+    project,apk = parse_parameters(sys.argv)
+    apktool_loading()
+    printManifest(apk)
+    executeGradlewSigning(project)
 
 
-apk_inspect = APK(apk_location)
+#function for loading apktool to the system
+#TODO maintanence for the jar version
+def apktool_loading():
+    scriptFile = urllib2.urlopen("https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/osx/apktool")
+    jarFile = urllib2.urlopen("https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.0.jar")
+    filename_script = "apktool"
+    filename_jar = "apktool.jar"
 
-print apk_inspect.cert_text
-print apk_inspect.file_md5
-print apk_inspect.cert_md5
-print apk_inspect.file_size
-print apk_inspect.androidversion
-print apk_inspect.package
-print apk_inspect.get_android_manifest_xml()
-print apk_inspect.get_android_manifest_axml()
-print apk_inspect.is_valid_APK()
-print apk_inspect.get_filename()
-print apk_inspect.get_package()
-print apk_inspect.get_androidversion_code()
-print apk_inspect.get_androidversion_name()
-print apk_inspect.get_max_sdk_version()
-print apk_inspect.get_min_sdk_version()
-print apk_inspect.get_target_sdk_version()
-print apk_inspect.get_libraries()
-print apk_inspect.get_files()
-print apk_inspect.get_files_types()
-    #print apkf.get_dex()
-print apk_inspect.get_main_activity()
-print apk_inspect.get_activities()
-print apk_inspect.get_services()
-print apk_inspect.get_receivers()
-print apk_inspect.get_providers()
-print apk_inspect.get_permissions()
-print apk_inspect.show()
+    with working_directory("/tmp"):
+        print "Downloading script..."
+        with open(filename_script, "wb") as output_script:
+            output_script.write(scriptFile.read())
+        print "Downloading jar file..."
+        with open(filename_jar, "wb") as output_jar:
+            output_jar.write(jarFile.read())
+        call(["mv",filename_script,"/usr/local/bin"])
+        call(["mv", filename_jar, "/usr/local/bin"])
 
-with working_directory(project_location):
-    call(["./gradlew","signingReport"])
+    with working_directory("/usr/local/bin"):
+        print "Changing the mod of the script..."
+        call(["chmod","+x",filename_script])
+        print "Changing the mod of the jar..."
+        call(["chmod", "+x", filename_jar])
+
+#This function parses paramenters
+def parse_parameters(argv):
+    apk_location = ''
+    project_location = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o",["-dir","-apk"])
+    except getopt.GetoptError:
+        print "initializer py -d <projectLocation> -a <apklocation>"
+        sys.exit(2)
+    for opt,arg in opts:
+        if opt in ("-d","--dir"):
+            project_location = arg
+            print "project location is parsed"
+        elif opt in ("-a","-apk"):
+            apk_location = arg
+            print "apk file location is parsed"
+    return project_location,apk_location
 
 
+def printManifest(apk_location):
+    with working_directory("/tmp"):
+        call(["apktool", "d", apk_location])
+    # Print AndroidManifest.xml file
+    with working_directory("/tmp" + "/app-release"):
+        f = open("AndroidManifest.xml", "rw")
+        for line in f:
+            print line,
+
+def executeGradlewSigning(project_location):
+    with working_directory(project_location):
+        call(["./gradlew", "signingReport"])
+
+
+# apk_location = "/users/ozgumusy/AndroidStudioProjects/RijksClient/app/app-release.apk"
+# project_location = "/users/ozgumusy/AndroidStudioProjects/RijksClient"
+
+
+
+
+# apk_inspect = APK(apk_location)
+
+# print apk_inspect.cert_text
+# print apk_inspect.file_md5
+# print apk_inspect.cert_md5
+# print apk_inspect.file_size
+# print apk_inspect.androidversion
+# print apk_inspect.package
+# print apk_inspect.get_android_manifest_xml()
+# print apk_inspect.get_android_manifest_axml()
+# print apk_inspect.is_valid_APK()
+# print apk_inspect.get_filename()
+# print apk_inspect.get_package()
+# print apk_inspect.get_androidversion_code()
+# print apk_inspect.get_androidversion_name()
+# print apk_inspect.get_max_sdk_version()
+# print apk_inspect.get_min_sdk_version()
+# print apk_inspect.get_target_sdk_version()
+# print apk_inspect.get_libraries()
+# print apk_inspect.get_files()
+# print apk_inspect.get_files_types()
+#     #print apkf.get_dex()
+# print apk_inspect.get_main_activity()
+# print apk_inspect.get_activities()
+# print apk_inspect.get_services()
+# print apk_inspect.get_receivers()
+# print apk_inspect.get_providers()
+# print apk_inspect.get_permissions()
+# print apk_inspect.show()
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
