58180c9edc1e87c65a55d7544ef5102d3d993f8d
ebsenol <ebsenol@gmail.com>
2016-08-17 16:20:10 +0300

    added my functions to checklist

--- scripts/CheckList.py
+++ scripts/CheckList.py
@@ -14,14 +14,32 @@ class Checklist(object):
         self.checklist_berker = berker.ChecklistBerker(self.project_dir,self.apk_dir)
 
     def executeTests(self):
+
         self.checklist_yigit.B2()
+        self.checklist_berker.B4()
         self.checklist_yigit.B5()
+        self.checklist_berker.B6()
         self.checklist_yigit.B7()
+        self.checklist_berker.B7()
         self.checklist_yigit.B8()
+        self.checklist_berker.B9()
+
         self.checklist_yigit.MAN1()
+        self.checklist_berker.MAN2()
         self.checklist_yigit.MAN3()
+        self.checklist_berker.MAN5()
+
         self.checklist_yigit.SIGN4()
+
+        self.checklist_berker.PERM2()
         self.checklist_yigit.PERM3()
+
+        self.checklist_berker.PRG2()
         self.checklist_yigit.PRG3()
+
         self.checklist_yigit.APK2()
+
+        self.checklist_berker.SEC1()
         self.checklist_yigit.SEC4()
+
+
